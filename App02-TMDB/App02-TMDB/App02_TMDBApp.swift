3396488f769177394e4570f1287d1ee80b08dbe7
Palacios427 <A01722533@tec.mx>
2024-08-20 12:56:24 -0600

    Xcode - The Movie Data Base

---
+++ App02-TMDB/App02-TMDB/App02_TMDBApp.swift
@@ -0,0 +1,35 @@
+//
+//  App02_TMDBApp.swift
+//  App02-TMDB
+//
+//  Created by David Cantú Delgado on 20/08/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct App02_TMDBApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    @State var moviesData: MoviesData = MoviesData()
+
+    var body: some Scene {
+        WindowGroup {
+            MoviesListView()
+                .environment(moviesData)
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ App02-TMDB/App02-TMDB/Model/Movie.swift
@@ -0,0 +1,43 @@
+//
+//  Movie.swift
+//  App02-TMDB
+//
+//  Created by David Cantú Delgado on 20/08/24.
+//
+
+import Foundation
+
+struct Movie: Identifiable, Codable {
+    var adult: Bool
+    var backdropPath: String
+    var genreIds: [Int]
+    var id: Int
+    var originalLanguage: String
+    var originalTitle: String
+    var overview: String
+    var popularity: Double
+    var posterPath: String
+    var releaseDate: String
+    var title: String
+    var video: Bool
+    var voteAverage: Double
+    var voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIds = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+
+}
