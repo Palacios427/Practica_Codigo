55cf991b93ffd550681b2c4d72727764abf290d4
Palacios427 <A01722533@tec.mx>
2024-08-21 12:19:55 -0600

    Added the Favorite function

---
+++ App02-TMDB/App02-TMDB/App02-TMDB/MoviesView.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesView.swift
+//  App02-TMDB
+//
+//  Created by Alumno on 21/08/24.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        NavigationStack {
+            TabView {
+                MoviesListView()
+                    .tabItem {
+                        Label("Movies", systemImage: "film")
+                    }
+                FavsListView()
+                    .tabItem {
+                        Label("Favorites",systemImage: "star.fill")
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    MoviesView()
+        .environment(MoviesData())
+}
